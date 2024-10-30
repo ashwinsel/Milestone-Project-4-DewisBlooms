11b722532693ec0c99375db946140ca9cd05259a
Ashwinkarthik Selveraj <b6rd31sv@students.codeinstitute.net>
2024-10-30 16:09:49 +0000

    update views.py in products folder and amend base.html to add search functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.urls import reverse
+from django.db.models import Q
 from .models import Product
 
 def all_products(request):
@@ -16,3 +19,25 @@ def product_detail(request, product_id):
         'product': product,
     }
     return render(request, 'products/product_detail.html', context)
+
+
+def all_products(request):
+    products = Product.objects.all()
+    query = None
+
+    # Check if there is a search query
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
+
+        # Use Q objects to allow searching in either name or description
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+    return render(request, 'products/products.html', context)
