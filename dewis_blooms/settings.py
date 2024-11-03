7ea6709cd2211eadb9a2c3ba32e969fd4e5c1ff2
Ashwinkarthik Selveraj <b6rd31sv@students.codeinstitute.net>
2024-11-03 19:25:17 +0000

    pdated settings for S3 static files

--- dewis_blooms/settings.py
+++ dewis_blooms/settings.py
@@ -134,11 +134,11 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
     AWS_S3_FILE_OVERWRITE = env.bool("AWS_S3_FILE_OVERWRITE", False)
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-    # Use S3 for file storage in production
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
     # Set URLs for static and media files hosted on S3
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
